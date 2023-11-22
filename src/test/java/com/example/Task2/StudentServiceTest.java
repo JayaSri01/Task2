7ce25c2a7d64351adc6a1b454748788a2fa6de1e
Jayasrilatha <ponnaganti.jayasrilatha@capgemini.com>
2023-11-22 23:52:01 +0530

    update

--- src/test/java/com/example/Task2/StudentServiceTest.java
+++ src/test/java/com/example/Task2/StudentServiceTest.java
@@ -2,13 +2,11 @@ package com.example.Task2;
 
 import com.example.Task2.entity.Student;
 import com.example.Task2.repository.StudentRepository;
-import com.example.Task2.service.StudentService;
 import com.example.Task2.service.StudentServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.internal.matchers.Null;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
@@ -27,32 +25,32 @@ import static org.mockito.Mockito.when;
         @Test
         public void testGetDetails() {
             List<Student> list=new ArrayList<>();
-            list.add(new Student(1,"venkatesh",24,10000));
-            list.add(new Student(2,"pavan",24,10000));
-            list.add(new Student(3,"srujan",24,10000));
-            list.add(new Student(4,"sahith",24,10000));
+            list.add(new Student(1,"jaya",24,90000));
+            list.add(new Student(2,"jasu",24,80000));
+            list.add(new Student(3,"veni",24,70000));
+            list.add(new Student(4,"bhavani",24,60000));
             when(sRepo.findAll()).thenReturn(list);
             List<Student> userExpected=service.getAllStudents();
             assertEquals(userExpected, list);
         }
         @Test
         public void testAddStudent() {
-            Student s=new Student(1,"venkatesh",25,100000);
+            Student s=new Student(1,"jaya",25,700000);
             when(sRepo.save(s)).thenReturn(s);
             Student s1=service.saveStudent(s);
             assertEquals(s.getId(), s1.getId());
         }
         @Test
         public void testGetById() {
-            Student s=new Student(1,"venkatesh",25,100000);
+            Student s=new Student(1,"jaya",25,700000);
             when(sRepo.findById(1)).thenReturn(Optional.of(s));
             Student s1=service.getStudentById(s.getId());
             assertEquals(s.getId(), s1.getId());
         }
         @Test
         public void testUpdate() {
-            Student s=new Student(1,"venkatesh",25,100000);
-            Student s1=new Student(2,"sathish",26,1000000);
+            Student s=new Student(1,"jaya",25,900000);
+            Student s1=new Student(2,"sri",26,8000000);
             when(sRepo.findById(1)).thenReturn(Optional.of(s));
             when(sRepo.save(s)).thenReturn(s);
             Student s2=service.updateStudent(1,s1);
@@ -60,7 +58,7 @@ import static org.mockito.Mockito.when;
         }
         @Test
         public void testDelete() {
-            Student s=new Student(2,"venkatesh",25,100000);
+            Student s=new Student(2,"sri",25,100000);
             int studentIdToDelete = s.getId();
             String result = service.deleteStudent(studentIdToDelete);
             assertEquals("Deleted",result);
